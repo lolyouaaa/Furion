012628a76d31367893631a3e6daedc34a60949d9
lolyou <lolyou@yeah.net>
2025-05-14 14:01:33 +0800

    add

---
+++ samples/Furion.Application/TestRouteMethod.cs
@@ -0,0 +1,16 @@
+﻿namespace Furion.Application;
+
+[Route("sys"), ApiDescriptionSettings(ForceWithRoutePrefix = true)]
+public class TestRouteMethod : IDynamicApiController
+{
+    [HttpGet("getDesc")]
+    public string GetDescription(string e)
+    {
+        return "Furion";
+    }
+
+    public string GetDescription2(string e)
+    {
+        return "Furion";
+    }
+}
